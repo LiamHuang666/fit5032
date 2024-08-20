dbb1a0f9735adbb1acadc2c524cdbc68cea64703
HUANG <hhua0076@student.monash.edu>
2024-08-20 13:40:31 +1000

    FIT5032-week5

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-// import JSON from './components/JSON.vue'
-import Form from './components/Form.vue'
+import JSONLab from './components/JSONLab.vue'
+import BHeader from './components/BHeader.vue'
+import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper"></div>
+  <header>
+    <BHeader />
   </header>
-  <JSON /> -->
 
-  <Form />
+  <main>
+    <LibraryRegistrationForm />
+    <!-- <JSONLab /> -->
+  </main>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
 }
 
@@ -42,5 +41,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
